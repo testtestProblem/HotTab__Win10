83a2cfbe6541bf6f5c20a96263ffef2e325ab21c
testtestproblem <tommyapple321@gmail.com>
2023-11-20 12:38:31 +0800

    Update - Add devicestate code, but not verify

--- HotTab_Win10/DeviceControlPage.xaml.cs
+++ HotTab_Win10/DeviceControlPage.xaml.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +17,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using static HotTab_Device2.DeviceState;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,9 +33,241 @@ namespace HotTab_Win10
             this.InitializeComponent();
         }
 
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            {
+                App.AppServiceConnected += MainPage_AppServiceConnected;
+                App.AppServiceDisconnected += MainPage_AppServiceDisconnected;
+                await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            }
+
+            //UWP will crash
+            //HasBeen_Click(0);
+
+        }
+
+        /// <summary>
+        /// When the desktop process is disconnected, reconnect if needed
+        /// </summary>
+        private async void MainPage_AppServiceDisconnected(object sender, EventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // disable UI to access the connection
+                //btnRegKey.IsEnabled = false;
+
+                // ask user if they want to reconnect
+                Reconnect();
+            });
+        }
+
+        /// <summary>
+        /// Ask user if they want to reconnect to the desktop process
+        /// </summary>
+        private async void Reconnect()
+        {
+            if (App.IsForeground)
+            {
+                MessageDialog dlg = new MessageDialog("Connection to desktop process lost. Reconnect?");
+                UICommand yesCommand = new UICommand("Yes", async (r) =>
+                {
+                    await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+                });
+                dlg.Commands.Add(yesCommand);
+                UICommand noCommand = new UICommand("No", (r) => { });
+                dlg.Commands.Add(noCommand);
+                await dlg.ShowAsync();
+            }
+        }
+
+
+        /// <summary>
+        /// When the desktop process is connected, get ready to send/receive requests
+        /// </summary>
+        private async void MainPage_AppServiceConnected(object sender, AppServiceTriggerDetails e)
+        {
+            App.Connection.RequestReceived += AppServiceConnection_RequestReceived;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                //enable UI to access the connection
+                //btnRegKey.IsEnabled = true;
+            });
+        }
+
+
+
+        private async void AppServiceConnection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                CheckDeviceState_Button((uint)args.Request.Message["deviceStateAll"]);
+            });
+        }
+
+        private void CheckDeviceState_Button(uint deviceStateCode)
+        {
+            if ((deviceStateCode & (uint)Modules.Wifi) == (uint)Modules.Wifi)
+            {
+                btn_wifi.Background = new SolidColorBrush(Windows.UI.Colors.Orange);
+            }
+            else
+            {
+                btn_wifi.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            }
+
+            if ((deviceStateCode & (uint)Modules.Gobi3G) == (uint)Modules.Gobi3G)
+            {
+                btn_gobi3G.Background = new SolidColorBrush(Windows.UI.Colors.Orange);
+            }
+            else
+            {
+                btn_gobi3G.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            }
+
+            if ((deviceStateCode & (uint)Modules.GPS) == (uint)Modules.GPS)
+            {
+                btn_GPS.Background = new SolidColorBrush(Windows.UI.Colors.Orange);
+            }
+            else
+            {
+                btn_GPS.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            }
+
+            if ((deviceStateCode & (uint)Modules.Bluetooth) == (uint)Modules.Bluetooth)
+            {
+                btn_bluetooth.Background = new SolidColorBrush(Windows.UI.Colors.Orange);
+            }
+            else
+            {
+                btn_bluetooth.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            }
+
+            if ((deviceStateCode & (uint)Modules.WebCamRear) == (uint)Modules.WebCamRear)
+            {
+                btn_webCamRear.Background = new SolidColorBrush(Windows.UI.Colors.Orange);
+            }
+            else
+            {
+                btn_webCamRear.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            }
+
+            if ((deviceStateCode & (uint)Modules.AllLED) == (uint)Modules.AllLED)
+            {
+                btn_allLED.Background = new SolidColorBrush(Windows.UI.Colors.Orange);
+            }
+            else
+            {
+                btn_allLED.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            }
+
+            if ((deviceStateCode & (uint)Modules.Barcode) == (uint)Modules.Barcode)
+            {
+                btn_barcode.Background = new SolidColorBrush(Windows.UI.Colors.Orange);
+            }
+            else
+            {
+                btn_barcode.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            }
+
+            if ((deviceStateCode & (uint)Modules.RFID) == (uint)Modules.RFID)
+            {
+                btn_RFID.Background = new SolidColorBrush(Windows.UI.Colors.Orange);
+            }
+            else
+            {
+                btn_RFID.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            }
+
+            if ((deviceStateCode & (uint)Modules.GPSAntenna) == (uint)Modules.GPSAntenna)
+            {
+                btn_GPSAntenna.Background = new SolidColorBrush(Windows.UI.Colors.Orange);
+            }
+            else
+            {
+                btn_GPSAntenna.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            }
+        }
+
+            private async void HasBeen_Click(uint moduleName)
+        {
+            ValueSet request = new ValueSet();
+            request.Add("deviceConfig", moduleName);
+            AppServiceResponse response = await App.Connection.SendMessageAsync(request);
+
+            if (response.Message["res_deviceConfig"] as uint? != null)
+            {
+                CheckDeviceState_Button((uint)response.Message["res_deviceConfig"]);
+            }
+        }
+
+
+
         private void return_btn_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void btn_wifi_Click(object sender, RoutedEventArgs e)
+        {
+            HasBeen_Click((uint)Modules.Wifi);
+        }
+
+        private void btn_gobi3G_Click(object sender, RoutedEventArgs e)
+        {
+            HasBeen_Click((uint)Modules.Gobi3G);
+        }
+
+        private void btn_GPS_Click(object sender, RoutedEventArgs e)
+        {
+            HasBeen_Click((uint)Modules.GPS);
+        }
+
+        private void btn_bluetooth_Click(object sender, RoutedEventArgs e)
+        {
+            HasBeen_Click((uint)Modules.Bluetooth);
+        }
+
+        private void btn_webCamRear_Click(object sender, RoutedEventArgs e)
+        {
+            HasBeen_Click((uint)Modules.WebCamRear);
+        }
+
+        private void btn_allLED_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_barcode_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_webCamFront_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_RFID_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_GPSAntenna_Click(object sender, RoutedEventArgs e)
+        {
+            HasBeen_Click((uint)Modules.GPSAntenna);
+        }
+
+        private void btn_expandUSB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_expandCOM_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ HotTab_Win10/DeviceState.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace HotTab_Device2
+{
+    class DeviceState
+    {
+        public enum Modules : uint
+        {
+            Wifi = 0x01,
+            Gobi3G = 0x2,
+            GPS = 0x4,
+            Bluetooth = 0x8,
+            WebCamRear = 0x20,
+            AllLED = 0x80,
+
+            Barcode = 0x100,
+            WebCam = 0x200,
+            RFID = 0x400,
+            GPSAntenna = 0x800,
+            ExpandUSB = 0x1000,
+            ExpandCOM = 0x2000
+        };
+        /*
+        public void ParseDeviceStatePowerCode(uint deviceStatePower)
+        {
+            if ((deviceStatePower & (uint)Modules.Wifi) == (uint)Modules.Wifi)
+            {
+                btn_wifi.Background = new ImageBrush
+                {
+                    ImageSource = new BitmapImage(new Uri(@"/Assets/device/enable/G_Wi-Fi.bmp", UriKind.RelativeOrAbsolute)),
+                    Stretch = Stretch.Uniform
+                };
+            }
+        }
+        */
+
+
+
+    }
+
+}
